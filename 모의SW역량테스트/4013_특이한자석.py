7ea688b216f5799155c7ade79f0a00ecd97784e3
chelseashin <chaewonshin95@gmail.com>
2019-08-28 21:38:39 +0900

    190828 | dfs_magnetic_problem

--- 모의SW역량테스트/4013_특이한자석.py
+++ 모의SW역량테스트/4013_특이한자석.py
@@ -5,9 +5,20 @@ def dfs(pos, dir):
     global mag, turn
     visited[pos] = 1
     # 양쪽에 있는 것들은 반대방향
+    if (pos > 0):
+        if mag[pos][6] != mag[pos-1][2] and visited[pos-1] == 0:
+            dfs(pos-1, -dir)
+    if (pos < 3):
+        if mag[pos][2] != mag[pos+1][6] and visited[pos+1] == 0:
+            dfs(pos+1, -dir)
 
-    # 같은 쪽에 있는 것들은 같은 방향
+    # 시계방향
+    if dir == 1:
+        mag[pos] = [mag[pos][-1]] + mag[pos][:-1]
 
+    # 반시계방향
+    else:
+        mag[pos] = mag[pos][1:] + [mag[pos][0]]
 
 
 T = int(input())
@@ -23,4 +34,4 @@ for tc in range(T):
         dfs(n-1, d)
 
     score = 1*mag[0][0] + 2*mag[1][0] + 4*mag[2][0] + 8*mag[3][0]
-    # print("#{} {}".format(tc+1, score))
+    print("#{} {}".format(tc+1, score))

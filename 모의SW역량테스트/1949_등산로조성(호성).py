aefc5f62ffcabed3292c8493d13684a217012f31
ChaewonShin <chaewonshin95@gmail.com>
2019-08-30 18:02:56 +0900

    190830 | my_algorithm_study_for_A

--- 모의SW역량테스트/1949_등산로조성(호성).py
+++ 모의SW역량테스트/1949_등산로조성(호성).py
@@ -40,7 +40,6 @@ for test_case in range(1, T+1):
     data = [list(map(int, input().split())) for _ in range(N)]
     visited = [[0 for _ in range(N)] for _ in range(N)]
 
-    top_check = []
     top = 0
     for i in range(N):
         for j in range(N):

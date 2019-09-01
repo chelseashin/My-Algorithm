82e99eec93860212e1d0a656ec92970d04223508
chelseashin <chaewonshin95@gmail.com>
2019-09-01 23:04:55 +0900

    190901 | algorithm_study

---
+++ baekjoon/06_야구(17281번).py
@@ -0,0 +1,55 @@
+import sys
+sys.stdin = open('06_input.txt')
+
+import itertools
+
+# 2
+# 4 0 0 0 1 1 1 0 0
+# 0 0 0 0 0 0 0 0 0
+
+N = int(input())
+arr = []
+for ining in range(N):
+    arr.append(list(map(int, input().split())))
+# print(arr)
+max_score = 0
+order = list(itertools.permutations(range(1, 9)))
+for o in order:
+    new = list(o)
+    new = new[0:3] + [0] + new[3:]
+    # print(new)
+    score = 0
+    for a in range(N):
+        A = []
+        for n in new:
+            A.append(arr[a][n])
+        # print(A)
+        for 
+
+    if score > max_score:
+        max_score = score
+
+print(max_score)
+
+
+# 순열 함수 - DFS
+# result = []
+# visited = [0] * 9
+# def dfs(depth):
+#     if depth == 9:
+#         print(result)
+#         return
+#     if depth == 3:
+#         result.append(0)
+#         dfs(depth + 1)
+#         result.pop()
+#     else:
+#         for i in range(1, 9):
+#             if visited[i]:
+#                 continue
+#             result.append(i)
+#             visited[i] = 1
+#             dfs(depth + 1)
+#             result.pop()
+#             visited[i] = 0
+# dfs(0)
--- 모의SW역량테스트/2115_벌꿀채집.py
+++ 모의SW역량테스트/2115_벌꿀채집.py
@@ -1,8 +1,52 @@
 import sys
 sys.stdin = open('2115_input.txt')
 
+# 열 선택
+# def combination(r, depth, k):
+#     if depth == 2:
+#         check(L)
+#         return
+#     for i in range(k, 6):
+#         L.append(i)
+#         combination(depth + 1, i)
+#         L.pop()
+
+# 행 선택
+def combination(depth, k):
+    if depth == 2:
+        # 꿀양 계산 해주는 함수 호출 여기서 해주면 됨.
+        check(L)
+        return
+    for i in range(k, 6):
+        L.append(i)
+        combination(depth + 1, i)
+        L.pop()
+
+def check(H):
+    global honey, N
+    if H[0] + H[1] > N:
+        return
+    else:
+        # H[0] : A가 선택한 행, H[1] : B가 선택한 행
+
+        if H[0] == H[1]:
+            for i in range(N-):
+                for j in range(i+1, N-M+1)
+                    cal(H[0][i])
+        else:
+            for i in range(N-M+1):
+                # honey[H[0]][i]
+                # 시작 좌표 r, c, 뽑을 수 있는 1 ~ M
+                cal(H[0], i, M) + cal(H[1], i, M)
+
 T = int(input())
 for tc in range(T):
     N, M, C = map(int, input().split())
     honey = [list(map(int, input().split())) for _ in range(N)]
-    print(honey)
+    total = 0
+    # 두 명의 양봉업자가 선택할 수 있는 행 : ex) L = [0, 0] ~ [N-1, N-1]
+    L = []
+    combination(2, 0, 0)
+
+
+    print("#{} {}".format(tc+1, total))

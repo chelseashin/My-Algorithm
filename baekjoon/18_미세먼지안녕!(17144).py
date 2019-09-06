1d232668eeb63ad5c546c70c0ca3da753d33af01
chelseashin <chaewonshin95@gmail.com>
2019-09-06 22:30:13 +0900

    190906 | goodbye! fine dust!

--- baekjoon/18_미세먼지안녕!(17144).py
+++ baekjoon/18_미세먼지안녕!(17144).py
@@ -7,21 +7,39 @@ import copy
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
-# 우상좌하, 우하좌상
-# cr = [[0, -1, 0, 1], [0, 1, 0, -1]]
-# cc = [[1, 0, -1, 0], [1, 0, -1, 0]]
-
+# 값 땡겨오는 방향
 # 상 우 하 좌, 하 우 상 좌
-jr = [[-1, 0, 1, 0], [-1, 0, 1, 0]]
+jr = [[-1, 0, 1, 0], [1, 0, -1, 0]]
 jc = [[0, 1, 0, -1], [0, 1, 0, -1]]
 
+# 공기청정기 - n은 (r, c)
 def clean(n):
-
+    global cleaner, R, C
+    r, c = cleaner[n]
+    dir = 0
     while True:
-        nr =
-        nc =
+        nr = r + jr[n][dir]
+        nc = c + jc[n][dir]
         if nr == cleaner[n][0] and nc == cleaner[n][1]:
+            new[r][c] =0
             break
+        # 아래부분
+        if n:
+        # 벽 만나면
+            if not (cleaner[n][0] <= nr < R and 0 <= nc < C):
+                dir += 1
+                continue
+        # 윗부분
+        else:
+            if not (0 <= nr < cleaner[n][0] + 1 and 0 <= nc < C):
+                dir += 1
+                continue
+        # 벽 만나지 않을 때
+        if new[r][c] != -1:
+            new[r][c] = new[nr][nc]
+        r = nr
+        c = nc
+
 def bfs():
     global arr, new, dust, R, C
     while dust:
@@ -49,16 +67,22 @@ for i in range(R):
 
 dust = []
 for t in range(T):
+    # 미세먼지 확산
     for i in range(R):
         for j in range(C):
             if arr[i][j] > 0:
                 dust.append((i, j))
                 bfs()
+
     # 공기청정
     for c in range(len(cleaner)):
         clean(c)
     arr = copy.deepcopy(new)
 
-print(new)
-
-# arr = copy.deepcopy(new)
+# print(arr)
+mise = 0
+for i in range(R):
+    for j in range(C):
+        if arr[i][j] > 0:
+            mise += arr[i][j]
+print(mise)

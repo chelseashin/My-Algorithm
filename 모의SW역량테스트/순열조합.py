0e026b2c99720be45ea8138c646999843f252a19
chelseashin <chaewonshin95@gmail.com>
2019-09-10 23:42:07 +0900

    190910 | my_algorithm_study

--- 모의SW역량테스트/순열조합.py
+++ 모의SW역량테스트/순열조합.py
@@ -1,26 +1,27 @@
 import itertools
 
+print('2개의 원소로 수열 만들기')
 pool = range(1, 10)
-# print(list(itertools.permutations(pool, 2)))  # 3개의 원소로 수열 만들기
-# print(list(map(''.join, itertools.permutations(pool, 2))))  # 2개의 원소로 수열 만들기
+print(list(itertools.permutations(pool, 2)))
 
-# print(list(itertools.combinations(pool, 3)))
+print('3개의 원소로 수열 만들기 - 조합. 순서 고려하지 않음')
+# print(list(map(''.join, itertools.permutations(pool, 2))))
 
-result =[]
+print(list(itertools.combinations(pool, 3)))
+
+result = []
 def dfs(depth):
     # if result[2] == 5:
     #     return
     if depth == 10:
-        check(result)
+        # check(result)
+        return
     for i in range(1, 10):
         result.append(i)
         dfs(depth + 1)
         result.pop()
 
-
-
-def check():
-    return
+print(result)
 
 
 
@@ -42,6 +43,7 @@ def check():
 
 ro = range(2, 5)
 print(list(itertools.permutations(ro, 2)))
+print('중복순열')
 print(list((itertools.product(ro, repeat=3))))
 # for i in range(2, 5):
 #     for j in range(2, 5):

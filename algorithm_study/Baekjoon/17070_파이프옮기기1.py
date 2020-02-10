ec937ebc3d552b62471538c5bb068f7181163f00
chelseashin <chaewonshin95@gmail.com>
2020-02-11 00:12:27 +0900

    200211 | algorithm study problems

---
+++ algorithm_study/Baekjoon/17070_파이프옮기기1.py
@@ -0,0 +1,40 @@
+import sys
+sys.stdin = open('17070_input.txt')
+
+def dfs(r, c, d):
+    global N, A, ans
+    # 도착 조건
+    if r == N-1 and c == N-1:
+        ans += 1
+        return
+
+    # 가로
+    if d == 1:
+        if c+1 < N and A[r][c+1] == 0:
+            dfs(r, c+1, 1)
+        if r+1 < N and c+1 < N and A[r][c+1] == 0 and A[r+1][c] == 0 and A[r+1][c+1] == 0:
+            dfs(r+1, c+1, 3)
+
+    # 세로
+    elif d == 2:
+        if r+1 < N and A[r+1][c] == 0:
+            dfs(r+1, c, 2)
+        if r+1 < N and c+1 < N and A[r][c+1] == 0 and A[r+1][c] == 0 and A[r+1][c+1] == 0:
+            dfs(r+1, c+1, 3)
+
+    # 대각선
+    else:
+        if c+1 < N and A[r][c+1] == 0:
+            dfs(r, c+1, 1)
+        if r+1 < N and A[r+1][c] == 0:
+            dfs(r+1, c, 2)
+        if r+1 < N and c+1 < N and A[r][c+1] == 0 and A[r+1][c] == 0 and A[r+1][c+1] == 0:
+            dfs(r+1, c+1, 3)
+
+
+N = int(input())
+A = [list(map(int, input().split())) for _ in range(N)]
+
+ans = 0
+dfs(0, 1, 1)
+print(ans)

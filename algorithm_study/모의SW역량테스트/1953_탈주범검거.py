ce4ad9cb58e8b67d2c45b577f2737abe0637eff9
chelseashin <chaewonshin95@gmail.com>
2020-02-23 11:48:32 +0900

    200223 | algorithm study

--- algorithm_study/모의SW역량테스트/1953_탈주범검거.py
+++ algorithm_study/모의SW역량테스트/1953_탈주범검거.py
@@ -1,9 +1,39 @@
 import sys
 sys.stdin = open("1953_input.txt")
 
+dr = (-1, 1, 0, 0)
+dc = (0, 0, -1, 1)
+
+IN = [[1, 2, 5, 6], [1, 2, 4, 7], [1, 3, 4, 5], [1, 3, 6, 7]]
+OUT = [[1, 2, 4, 7], [1, 2, 5, 6], [1, 3, 6, 7], [1, 3, 4, 5]]
+
+def bfs(sr, sc):
+    global N, M, L, info, cnt
+    visited = [[0] * M for _ in range(N)]
+    cnt = 0
+    visited[sr][sc] = 1
+    Q = [(sr, sc)]
+    while Q:
+        r, c = Q.pop(0)
+        cnt += 1
+        for i in range(4):
+            if info[r][c] not in OUT[i]:
+                continue
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if not (0 <= nr < N and 0 <= nc < M):
+                continue
+            if info[nr][nc] == 0:
+                continue
+            if info[nr][nc] in IN[i] and visited[nr][nc] == 0:
+                visited[nr][nc] = visited[r][c] + 1
+                if visited[nr][nc] <= L:
+                    Q.append((nr, nc))
+
 T = int(input())
 for tc in range(T):
     N, M, R, C, L = map(int, input().split())
     info = [list(map(int, input().split())) for _ in range(N)]
-    ans = 0
-    print("#{} {}".format(tc+1, ans))
+    bfs(R, C)
+
+    print("#{} {}".format(tc+1, cnt))

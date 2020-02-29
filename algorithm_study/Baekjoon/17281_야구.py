79c44a15695720cd205c60afa999fd9abcd9bfbf
chelseashin <chaewonshin95@gmail.com>
2020-02-29 23:49:04 +0900

    200229 | algorithm study

--- algorithm_study/Baekjoon/17281_야구.py
+++ algorithm_study/Baekjoon/17281_야구.py
@@ -1,7 +1,29 @@
 import sys
 sys.stdin = open("17281_input.txt")
 
+def perm(depth):
+    if depth == 9:
+        print(order)
+        return
+    if depth == 3:
+        order.append(0)
+        perm(depth + 1)
+        order.pop()
+    else:
+        for i in range(1, 9):
+            if visited[i]:
+                continue
+            visited[i] = 1
+            order.append(i)
+            perm(depth+1)
+            order.pop()
+            visited[i] = 0
+
 N = int(input())
 for _ in range(N):
     result = list(map(int, input().split()))
-    print(result)
+    # print(result)
+
+visited = [0] * 9
+order = []
+perm(0)

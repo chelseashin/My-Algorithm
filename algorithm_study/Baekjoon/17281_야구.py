ebb664942657020bf673c7c169f74a6fe68d8bba
chelseashin <chaewonshin95@gmail.com>
2020-03-01 15:51:48 +0900

    200301 | algorithm problems

--- algorithm_study/Baekjoon/17281_야구.py
+++ algorithm_study/Baekjoon/17281_야구.py
@@ -1,10 +1,40 @@
 import sys
 sys.stdin = open("17281_input.txt")
 
+from itertools import permutations
+
+def game(order):
+    global inning
+    hitter_idx = 0
+    score = 0
+    for each_inning in inning:
+        outcount = 0
+        b1, b2, b3 = 0, 0, 0
+        while outcount < 3:
+            if each_inning[order[hitter_idx]] == 0:
+                outcount += 1
+            elif each_inning[order[hitter_idx]] == 1:
+                score += b3
+                b1, b2, b3 = 1, b1, b2
+            elif each_inning[order[hitter_idx]] == 2:
+                score += (b2 + b3)
+                b1, b2, b3 = 0, 1, b1
+            elif each_inning[order[hitter_idx]] == 3:
+                score += (b1 + b2 + b3)
+                b1, b2, b3 = 0, 0, 1
+            elif each_inning[order[hitter_idx]] == 4:
+                score += (b1 + b2 + b3 + 1)
+                b1, b2, b3 = 0, 0, 0
+
+            hitter_idx = (hitter_idx + 1) % 9
+    return score
+
 def perm(depth):
+    global result, total_count, cnt, MAX
     if depth == 9:
-        print(order)
-        return
+        MAX = max(MAX, game(order))
+        # print(order)
+        return MAX
     if depth == 3:
         order.append(0)
         perm(depth + 1)
@@ -20,10 +50,17 @@ def perm(depth):
             visited[i] = 0
 
 N = int(input())
-for _ in range(N):
-    result = list(map(int, input().split()))
-    # print(result)
+inning = [list(map(int, input().split())) for _ in range(N)]
+MAX = float('-inf')
 
+# 방법 1
 visited = [0] * 9
 order = []
-perm(0)
+# perm(0)
+
+# 방법 2
+for line_up in permutations(range(1, 9), 8):
+    line_up = list(line_up[:3]) + [0] + list(line_up[3:])
+    MAX = max(MAX, game(line_up))
+
+print(MAX)

acea53b4b5ccfa19a7aac567b88cbbb81779fe76
chelseashin <chaewonshin95@gmail.com>
2020-08-05 01:26:31 +0900

    200805 | Keep going Chelsea!

--- baekjoon/삼성A형기출문제/16637_괄호추가하기.py
+++ baekjoon/삼성A형기출문제/16637_괄호추가하기.py
@@ -1,21 +1,50 @@
 import sys
 sys.stdin = open('16637_input.txt')
+from collections import deque
 
+def cal(n1, op, n2):
+    if op == '+': return n1 + n2
+    if op == '-': return n1 - n2
+    if op == '*': return n1 * n2
+#
+def calculate():
+    global S
+    Q = deque()
+    i = 0
+    while True:
+        if i == N:
+            break
+        if i%2 and check[i]:
+            Q.append(cal(int(Q.pop()), S[i], int(S[i+1])))
+            i += 1
+        else:
+            Q.append(S[i])
+        i += 1
+    while Q:
+        if len(Q) == 1:
+            break
+        Q.appendleft(cal(int(Q.popleft()), Q.popleft(), int(Q.popleft())))
+    return Q[0]
 
-def dfs(pos=1):
+def dfs(pos):
     global MAX
     if pos >= N:
-        print(check)
+        # print(check)
+        temp = calculate()
+        # print(temp)
+        if temp > MAX:
+            MAX = temp
         return
     check[pos] = 1
-    MAX = max(MAX, dfs(pos+4))
+    dfs(pos+4)
     check[pos] = 0
-    MAX = max(MAX, dfs(pos+2))
-    return MAX
+    dfs(pos+2)
 
 N = int(input())
 S = input()
 MAX = -2**31
 check = [0] * N
 
-print(dfs())
+dfs(1)
+print(MAX)
+# print(MAX)

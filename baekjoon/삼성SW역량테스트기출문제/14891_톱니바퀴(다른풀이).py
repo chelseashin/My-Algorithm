3e0cce243d7f0cdf1c9e8349976727663286acf8
chelseashin <chaewonshin95@gmail.com>
2020-08-23 01:11:36 +0900

    200823 | Algorithm Study continue

--- baekjoon/삼성SW역량테스트기출문제/14891_톱니바퀴(다른풀이).py
+++ baekjoon/삼성SW역량테스트기출문제/14891_톱니바퀴(다른풀이).py
@@ -1,36 +1,41 @@
 import sys
 sys.stdin = open('14891_input.txt')
 
-from collections import deque
-
-# 양쪽 톱니바퀴만 생각해서 동작하는 dfs로 구현
-#
-
-def dfs(pos, dir):
-    turn[pos] = 1
-    if pos < 3:
-        if gears[pos][2] != gears[pos+1][6] and not turn[pos+1]:
-            dfs(pos+1, -dir)
-    if (pos > 0):
-        if gears[pos][6] != gears[pos-1][2] and not turn[pos-1]:
-            dfs(pos-1, -dir)
-      # 1: 시계방향, -1: 반시계방향
-    if dir == 1:
-        gears[pos].rotate()
-    else:
-        gears[pos].rotate(-1)
+# 직관적으로 현 위치 기준으로 오른쪽, 왼쪽을 나누어
+# 회전하는 바퀴와 방향을 기록하는 direction 변수를 만듦
+# 리스트 슬라이싱을 이용하여 회전 구현
+# 비트 연산 활용하여 합계 점수 구하기
 
 # main
-gears = [deque(input()) for _ in range(4)]
+gears = [list(input()) for _ in range(4)]
 K = int(input())
 rotation = [list(map(int, input().split())) for _ in range(K)]
 
 for num, dir in rotation:
-    turn = [0] * 4
-    dfs(num-1, dir)
+    direction = [0] * 4
+    direction[num-1] = dir
+    # 오른쪽 방향
+    for i in range(num-1, 3):
+        if gears[i][2] != gears[i+1][6]:
+            direction[i+1] = -direction[i]
+    # 왼쪽 방향
+    for i in range(num-1, 0, -1):
+        if gears[i][6] != gears[i-1][2]:
+            direction[i-1] = -direction[i]
+    # print(direction)
+
+    # 회전
+    for i in range(4):
+        # 시계방향
+        if direction[i] == 1:
+            gears[i] = [gears[i][-1]] + gears[i][:-1]
+        # 반시계방향
+        elif direction[i] == -1:
+            gears[i] = gears[i][1:] + [gears[i][0]]
+# print(gears)
 
 score = 0
 for i in range(4):
     if gears[i][0] == '1':
-        score += 1 << i
+        score += (1 << i)
 print(score)

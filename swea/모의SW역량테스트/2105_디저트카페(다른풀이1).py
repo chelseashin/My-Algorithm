a2e9eff9e885ce47dbdd854935617623866379ef
chelseashin <chaewonshin95@gmail.com>
2020-08-28 23:49:44 +0900

    208028 | Algorithm Study Keep Going

---
+++ swea/모의SW역량테스트/2105_디저트카페(다른풀이1).py
@@ -0,0 +1,59 @@
+import sys
+sys.stdin = open('2105_input.txt')
+
+# 다른 풀이
+
+# 각 변의 길이를 num 리스트에 넣어서
+# 마주보는 변의 길이가 같을 때
+# 가능한 경우인지 check 함수로 확인
+
+# 우하, 좌하, 좌상, 우상
+dr = (1, 1, -1, -1)
+dc = (1, -1, -1, 1)
+
+def check(r, c):
+    global MAX, M
+    temp = []
+    cnt = 0
+    for i in range(4):
+        for j in range(num[i]):
+            r = r + dr[i]
+            c = c + dc[i]
+            if M[r][c] not in temp:
+                temp.append(M[r][c])
+                cnt += 1
+            else:
+                return
+    MAX = max(MAX, cnt)
+
+def dfs(dir, r, c):
+    if dir > 2:
+        if num[0] != num[2]:
+            return
+
+    if dir == 4:
+        if num[0] == num[2] and num[1] == num[3]:
+            check(sr, sc)
+        return
+
+    for dis in range(N-1, 0, -1):
+        nr = r + dr[dir] * dis
+        nc = c + dc[dir] * dis
+        if (0 <= nr < N and 0 <= nc < N):
+            num[dir] = dis
+            dfs(dir+1, nr, nc)
+
+# main
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    M = [list(map(int, input().split())) for _ in range(N)]
+    num = [0, 0, 0, 0]
+    MAX = -1
+
+    for i in range(N-2):
+        for j in range(1, N-1):
+            sr, sc = i, j
+            dfs(0, i, j)
+
+    print("#{} {}".format(tc + 1, MAX))
---
+++ swea/모의SW역량테스트/2105_디저트카페.py
@@ -0,0 +1,76 @@
+import sys
+sys.stdin = open('2105_input.txt')
+
+# 우하, 좌하, 좌상, 우상
+dr = (1, 1, -1, -1)
+dc = (1, -1, -1, 1)
+
+# 시작 좌표, 방향, 들린 카페 리스트, 우하향, 좌하향
+def dfs(row, col, dir, dis1, dis2, cafe):
+    global MAX
+    if dir == 4:
+        MAX = max(MAX, len(cafe))
+        return
+    if dir < 2:
+        while True:
+            if dir == 0:
+                dis1 += 1
+            if dir == 1:
+                dis2 += 1
+            row += dr[dir]
+            col += dc[dir]
+            if not (0 <= row < N and 0 <= col < N):
+                return
+            if raw[row][col] in cafe:
+                return
+            cafe += (raw[row][col],)
+            dfs(row, col, dir+1, dis1, dis2, cafe)
+
+    elif dir == 2:
+        for _ in range(1, dis1+1):
+            row += dr[dir]
+            col += dc[dir]
+            if not (0 <= row < N and 0 <= col < N):
+                return
+            if raw[row][col] in cafe:
+                return
+            cafe += (raw[row][col],)
+        dfs(row, col, dir+1, dis1, dis2, cafe)
+
+    elif dir == 3:
+        for _ in range(1, dis2):
+            row += dr[dir]
+            col += dc[dir]
+            if not (0 <= row < N and 0 <= col < N):
+                return
+            if raw[row][col] in cafe:
+                return
+            cafe += (raw[row][col],)
+        dfs(row, col, dir+1, dis1, dis2, cafe)
+    return
+
+# main
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    raw = [list(map(int, input().split())) for _ in range(N)]
+    MAX = -1
+
+    # 시작 위치는 각 벽의 끝, 아래에서 2번째까지는 될 수 없다.
+    # 모서리로는 갈 수가 없다.
+    for i in range(N-2):
+        for j in range(1, N-1):
+            dfs(i, j, 0, 0, 0, (raw[i][j],))
+
+    print("#{} {}".format(tc+1, MAX))
+
+    # 1 6
+    # 2 -1
+    # 3 4
+    # 4 4
+    # 5 8
+    # 6 6
+    # 7 14
+    # 8 12
+    # 9 18
+    # 10 30

4c5cc115bdc2718918ea88dcbca9001ab1efab35
chelseashin <chaewonshin95@gmail.com>
2020-11-22 22:42:32 +0900

    201122 | Algorithm Study restart

--- baekjoon/weekly_study/2346_풍선터뜨리기.py
+++ baekjoon/weekly_study/2346_풍선터뜨리기.py
@@ -1,8 +1,21 @@
-from collections import deque
 N = 5
-papers = deque([3, 2, 1, -3, -1])
+P = [3, 2, 1, -3, -1]
 
+from collections import deque
 # N = int(input())
-# B = deque(map(int, input().split()))
-print(N, papers)
+# P = list(map(int, input().split()))
+Q = deque([(i, P[i-1]) for i in range(1, N+1)])
+# print(Q)
 
+while True:
+    print(Q)
+    idx, x = Q.popleft()
+    print(idx, end=" ")
+    if not Q:
+        break
+    if x > 0:
+        for i in range(x-1):
+            Q.append(Q.popleft())
+    else:
+        for i in range(abs(x)):
+            Q.appendleft(Q.pop())

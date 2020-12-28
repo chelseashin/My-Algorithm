a70aac0c847436dc91d9464e869085916ee0f8ca
chelseashin <chaewonshin95@gmail.com>
2020-12-28 23:48:17 +0900

    201228 | Algorithm Problem Solving

---
+++ baekjoon/my_study/1022_소용돌이예쁘게출력하기.py
@@ -0,0 +1,44 @@
+import sys
+sys.stdin = open('1022_input.txt')
+input = sys.stdin.readline
+
+r1, c1, r2, c2 = map(int, input().split())
+# print(r1, c1, r2, c2)
+
+# 우 상 좌 하
+dr = (0, -1, 0, 1)
+dc = (1, 0, -1, 0)
+
+
+A = [[0] * (c2 - c1 + 1) for _ in range(r2 - r1 + 1)]
+total = (r2 - r1 + 1) * (c2 - c1 + 1)
+# 시작점 표시
+sr, sc = -1 * r1, -1 * c1
+A[sr][sc] = 1
+total -= 1
+direction = 0
+dis = 1
+num = 1
+while total:
+    for i in range(2):
+        for j in range(dis):
+            num += 1
+            nr = sr + dr[direction]
+            nc = sc + dc[direction]
+            sr, sc = nr, nc
+            # print(dis, j, num, '방향', direction, '현 위치', (nr, nc), total)
+            if not total:
+                break
+            if not (0 <= nr < (r2 - r1 + 1) and 0 <= nc < (c2 - c1 + 1)):
+                continue
+            A[nr][nc] = num
+            total -= 1
+        direction = (direction + 1) % 4
+    dis += 1
+
+for a in A:
+    print(*a)
+# for d in A:
+#     for i in d:
+#         print(i, end=' ')
+#     print()

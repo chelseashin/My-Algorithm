19667b40116864ceff548b9645f32de0bb1ad37d
chelseashin <chaewonshin95@gmail.com>
2021-01-01 23:58:21 +0900

    210101 | Algorithm Problem Solving

--- baekjoon/weekly_study/삼성SW역량테스트기출문제/14503_로봇청소기.py
+++ baekjoon/weekly_study/삼성SW역량테스트기출문제/14503_로봇청소기.py
@@ -1,3 +1,4 @@
+# 1h 40m
 import sys
 sys.stdin = open("14503_input.txt")
 input = sys.stdin.readline
@@ -5,17 +6,32 @@ input = sys.stdin.readline
 # 북 동 남 서
 dr = (-1, 0, 1, 0)
 dc = (0, 1, 0, -1)
+chd = (3, 0, 1, 2)
 
 # main
 N, M = map(int, input().split())
 r, c, d = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0] * M for _ in range(N)]
-print(N, M, r, c, d)
-cnt = 0
-
-# for a in A:
-#     print(a)
-#
-# for v in visited:
-#     print(v)
+A[r][c] = 2
+cnt = 1
+while True:
+    for i in range(4):
+        d = chd[d]
+        nr = r + dr[d]
+        nc = c + dc[d]
+        if A[nr][nc]:
+            continue
+        if not A[nr][nc]:
+            cnt += 1
+            A[nr][nc] = 2   # 청소공간 표시
+            r, c = nr, nc   # 이동
+            break
+    else:                   # 위 for 문에서 아무것도 걸리지 않으면 후진 실행
+        nr = r - dr[d]
+        nc = c - dc[d]
+        if A[nr][nc] == 2:
+            r, c = nr, nc
+            continue
+        elif A[nr][nc] == 1:    # 뒤쪽 방향 벽이면 후진 X
+            print(cnt)
+            break

4b3f940100a71355d0e257eaa5fd23a1f1657c33
chelseashin <chaewonshin95@gmail.com>
2021-01-03 23:55:42 +0900

    210103 | Alogrithm Problem Solving

--- baekjoon/weekly_study/삼성SW역량테스트기출문제/14889_스타트와링크.py
+++ baekjoon/weekly_study/삼성SW역량테스트기출문제/14889_스타트와링크.py
@@ -2,6 +2,40 @@ import sys
 sys.stdin = open("14889_input.txt")
 input = sys.stdin.readline
 
+def NChooseK(n, k):
+    numerator = 1
+    denominator = 1
+    k = min(n-k, k) #조합의 대칭성을 이용하여 더 적은 수의 연산을 하기 위해서입니다.
+    for i in range(1, k+1):
+        denominator *= i
+        numerator *= n+1-i
+    return numerator/denominator
+
+def comb(depth, k):
+    global MIN, half, cnt, finish
+    if cnt >= finish:
+        return
+    if depth == half:
+        # 최솟값 갱신
+        power = [0, 0]
+        for i in range(N):
+            for j in range(N):
+                if team[i] == team[j]:
+                    power[team[i]] += S[i][j]
+        MIN = min(MIN, abs(power[0]-power[1]))
+        cnt += 1
+        return
+    for i in range(k, N):
+        team[i] = 1
+        comb(depth+1, i+1)
+        team[i] = 0
+
 N = int(input())
 S = [list(map(int, input().split())) for _ in range(N)]
-print(N, S)
+cnt = 0
+half = N // 2
+finish = NChooseK(N, half) / 2
+MIN = float('inf')
+team = [0] * N
+comb(0, 0)
+print(MIN)

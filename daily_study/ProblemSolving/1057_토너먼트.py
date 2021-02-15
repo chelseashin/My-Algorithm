f4e925fc3c93c42d80314198e6c96626cacf2652
chelseashin <chaewonshin95@gmail.com>
2021-02-15 23:59:16 +0900

    210215 | Algorithm Problem Solving

--- daily_study/ProblemSolving/1057_토너먼트.py
+++ daily_study/ProblemSolving/1057_토너먼트.py
@@ -3,4 +3,32 @@ input = sys.stdin.readline
 
 # 참가자 수, 지민 번호, 한수 번호
 N, J, H = map(int, input().split())
-print(N, J, H)
+
+# 방법 1
+# cnt = 0
+# while J != H:   # 두 사람이 같지 않을 동안 계속 시행
+#     J -= J // 2
+#     H -= H // 2
+#     cnt += 1
+# print(cnt)
+
+
+# 방법 2
+# 번호 홀수이면 + 1, 짝수이면 그대로 리턴
+def idx(n):
+    if n % 2:
+        return n+1
+    else:
+        return n
+
+cnt = 0
+while True:
+    cnt += 1
+    J = idx(J)
+    H = idx(H)
+    if J == H:
+        print(cnt)
+        break
+    # 다음 라운드로 넘어갈 때는 나누기 2한 번호가 됨
+    J //= 2
+    H //= 2
---
+++ programmers/Heap/이중우선순위큐.py
@@ -0,0 +1,79 @@
+# 17:00 start
+# 17:27 finish
+
+# Heap을 이용해 개선한 풀이 - heap 한 개만 써서 가장 깔끔하고 직관적
+import heapq
+
+def solution(operations):
+    heap = []
+    for operation in operations:
+        cmd, num = operation.split()
+        if cmd == 'I':
+            heapq.heappush(heap, int(num))
+        elif heap:
+            if num == '1':
+                # 원소의 remove가 가능한 이유는 힙의 근간이 리스트이기 때문!
+                heap.remove(max(heap))
+            else:
+                heapq.heappop(heap)
+    if not heap:
+        return [0, 0]
+    else:
+        return [max(heap), min(heap)]
+
+# 최소 힙, 최대 힙 따로 사용한 코드
+# 추가 설명 : 항상 최대힙과 최소힙은 길이가 같으므로
+# 최대힙 or 최소힙이 바닥나는 경우는 모든 숫자를 다 썼을때 입니다.
+# 하지만, 만약 숫자가 총 4개가 들어갔고 최대힙, 최소힙에 각각 2번의 삭제명령이 있었다면
+# 원래 빈 리스트가 되어야 하므로,
+# 뒤에 -max_heap[0](2번째로 작았던 수)와 min_heap[0](2번째로 컸던 수)를 비교하는 조건을 추가해 주신 것 같습니다!
+
+# def solution(operations):
+#     max_heap = []
+#     min_heap = []
+#     for operation in operations:
+#         cmd, num = operation.split()
+#         if cmd == "I":
+#             heapq.heappush(max_heap, -int(num))
+#             heapq.heappush(min_heap, int(num))
+#         else:
+#             # print(cmd, num, "연산 전", min_heap, max_heap)
+#             if num == "1":  # 최댓값 삭제
+#                 if max_heap:
+#                     heapq.heappop(max_heap)
+#                     if not max_heap or -max_heap[0] < min_heap[0]:
+#                         min_heap, max_heap = [], []
+#                     # print(cmd, num, "최댓값 제거 연산 후", min_heap, max_heap)
+#             else:           # 최솟값 삭제
+#                 if min_heap:
+#                     heapq.heappop(min_heap)
+#                     if not min_heap or -max_heap[0] < min_heap[0]:
+#                         min_heap, max_heap = [], []
+#                     # print(cmd, num, "최솟값 제거 연산 후", min_heap, max_heap)
+#     if not min_heap:
+#         return [0, 0]
+#     return [-heapq.heappop(max_heap), heapq.heappop(min_heap)]
+
+# 처음 풀이
+# Heap 안 쓰고 List로 개떡같이 짰는데 TC가 적어서인지 통과됨..
+# def solution(operations):
+#     Q = []
+#     for operation in operations:
+#         cmd, num = operation.split()
+#         if cmd == 'I':
+#             Q.append(int(num))
+#         else:
+#             Q.sort()
+#             if Q:
+#                 if num == '1':
+#                     Q.pop()
+#                 else:
+#                     Q.pop(0)
+#     if not Q:
+#         return [0, 0]
+#     return [max(Q), min(Q)]
+
+# print(solution(["I 16", "D 1"]))
+# print(solution(["I 7", "I 5", "I -5", "D -1"]))
+# print(solution(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]))
+print(solution(["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]))

800c03c7d0d36ec2dc94969cb5d4819cc789b776
chelseashin <chaewonshin95@gmail.com>
2021-02-19 01:18:17 +0900

    210219 | Algorithm Problem Solving

--- programmers/Heap/디스크컨트롤러.py
+++ programmers/Heap/디스크컨트롤러.py
@@ -6,53 +6,53 @@
 from heapq import heappop, heappush
 
 # 다른 풀이 1
-# def solution(jobs):
-#     cnt, last, answer = 0, -1, 0
-#     jlen = len(jobs)
-#     heap = []
-#     jobs.sort()
-#     time = jobs[0][0]   # 시작시간 초기화
-#     while cnt < jlen:
-#         for s, t in jobs:
-#             if last < s <= time:
-#                 # 작업 소요시간으로 최소힙 만들기 (t, s)로 push
-#                 heappush(heap, (t, s))
-#         # 바로 수행할 수 있는 작업이 있는 경우
-#         if len(heap) > 0:
-#             cnt += 1
-#             last = time
-#             term, start = heappop(heap)
-#             time += term
-#             answer += (time - start)
-#         # 바로 수행할 수 있는 작업이 없는 경우
-#         else:
-#             time += 1
-#
-#     return answer // cnt
+def solution(jobs):
+    cnt, last, answer = 0, -1, 0
+    jlen = len(jobs)
+    heap = []
+    jobs.sort()
+    time = jobs[0][0]   # 시작시간 초기화
+    while cnt < jlen:
+        for s, t in jobs:
+            if last < s <= time:
+                # 작업 소요시간으로 최소힙 만들기 (t, s)로 push
+                heappush(heap, (t, s))
+        # 바로 수행할 수 있는 작업이 있는 경우
+        if len(heap) > 0:
+            cnt += 1
+            last = time
+            term, start = heappop(heap)
+            time += term
+            answer += (time - start)
+        # 바로 수행할 수 있는 작업이 없는 경우
+        else:
+            time += 1
+
+    return answer // cnt
 
 
 # 다른 풀이 2 - Heap 사용한 것보다 압도적으로 빨리 수행됨
 # 참고 링크 : https://johnyejin.tistory.com/132
 # 아니.. 너무 어렵다 이해하기가.. 역량 부족인 듯
-def solution(jobs):
-    answer = 0
-    start = 0
-    N = len(jobs)
-    jobs.sort(key=lambda x: x[1])
-    print(jobs)
-    while jobs:
-        for i in range(len(jobs)):
-            # 작업 수행할 수 있으면
-            if jobs[i][0] <= start:     # 시작시간 조건
-                start += jobs[i][1]     # 소요시간 조건
-                answer += start - jobs[i][0]
-                jobs.pop(i)
-                break
-            # 해당 시점에 작업 들어오지 않았으면 시간 += 1
-            if i == len(jobs) - 1:
-                start += 1
-        print(start, answer, jobs)
-    return answer // N
+# def solution(jobs):
+#     answer = 0
+#     start = 0
+#     N = len(jobs)
+#     jobs.sort(key=lambda x: x[1])
+#     print(jobs)
+#     while jobs:
+#         for i in range(len(jobs)):
+#             # 작업 수행할 수 있으면
+#             if jobs[i][0] <= start:     # 시작시간 조건
+#                 start += jobs[i][1]     # 소요시간 조건
+#                 answer += start - jobs[i][0]
+#                 jobs.pop(i)
+#                 break
+#             # 해당 시점에 작업 들어오지 않았으면 시간 += 1
+#             if i == len(jobs) - 1:
+#                 start += 1
+#         print(start, answer, jobs)
+#     return answer // N
 
 print(solution([[0, 3], [1, 9], [2, 6]]))               # 9
 # print(solution([[0, 3], [4, 3], [10, 3]]))              # 3
--- programmers/Heap/이중우선순위큐.py
+++ programmers/Heap/이중우선순위큐.py
@@ -1,25 +1,27 @@
 # 17:00 start
 # 17:27 finish
 
+# 이진탐색으로 DP 구현 https://jason9319.tistory.com/113
+
 # Heap을 이용해 개선한 풀이 - heap 한 개만 써서 가장 깔끔하고 직관적
 import heapq
 
-def solution(operations):
-    heap = []
-    for operation in operations:
-        cmd, num = operation.split()
-        if cmd == 'I':
-            heapq.heappush(heap, int(num))
-        elif heap:
-            if num == '1':
-                # 원소의 remove가 가능한 이유는 힙의 근간이 리스트이기 때문!
-                heap.remove(max(heap))
-            else:
-                heapq.heappop(heap)
-    if not heap:
-        return [0, 0]
-    else:
-        return [max(heap), min(heap)]
+# def solution(operations):
+#     heap = []
+#     for operation in operations:
+#         cmd, num = operation.split()
+#         if cmd == 'I':
+#             heapq.heappush(heap, int(num))
+#         elif heap:
+#             if num == '1':
+#                 # 원소의 remove가 가능한 이유는 힙의 근간이 리스트이기 때문!
+#                 heap.remove(max(heap))
+#             else:
+#                 heapq.heappop(heap)
+#     if not heap:
+#         return [0, 0]
+#     else:
+#         return [max(heap), heapq.heappop(heap)]
 
 # 최소 힙, 최대 힙 따로 사용한 코드
 # 추가 설명 : 항상 최대힙과 최소힙은 길이가 같으므로
@@ -28,31 +30,31 @@ def solution(operations):
 # 원래 빈 리스트가 되어야 하므로,
 # 뒤에 -max_heap[0](2번째로 작았던 수)와 min_heap[0](2번째로 컸던 수)를 비교하는 조건을 추가해 주신 것 같습니다!
 
-# def solution(operations):
-#     max_heap = []
-#     min_heap = []
-#     for operation in operations:
-#         cmd, num = operation.split()
-#         if cmd == "I":
-#             heapq.heappush(max_heap, -int(num))
-#             heapq.heappush(min_heap, int(num))
-#         else:
-#             # print(cmd, num, "연산 전", min_heap, max_heap)
-#             if num == "1":  # 최댓값 삭제
-#                 if max_heap:
-#                     heapq.heappop(max_heap)
-#                     if not max_heap or -max_heap[0] < min_heap[0]:
-#                         min_heap, max_heap = [], []
-#                     # print(cmd, num, "최댓값 제거 연산 후", min_heap, max_heap)
-#             else:           # 최솟값 삭제
-#                 if min_heap:
-#                     heapq.heappop(min_heap)
-#                     if not min_heap or -max_heap[0] < min_heap[0]:
-#                         min_heap, max_heap = [], []
-#                     # print(cmd, num, "최솟값 제거 연산 후", min_heap, max_heap)
-#     if not min_heap:
-#         return [0, 0]
-#     return [-heapq.heappop(max_heap), heapq.heappop(min_heap)]
+def solution(operations):
+    max_heap = []
+    min_heap = []
+    for operation in operations:
+        cmd, num = operation.split()
+        if cmd == "I":
+            heapq.heappush(max_heap, -int(num))
+            heapq.heappush(min_heap, int(num))
+        else:
+            print(cmd, num, "연산 전", min_heap, max_heap)
+            if num == "1":  # 최댓값 삭제
+                if max_heap:
+                    heapq.heappop(max_heap)
+                    if not max_heap or -max_heap[0] < min_heap[0]:
+                        min_heap, max_heap = [], []
+                    print(cmd, num, "최댓값 제거 연산 후", min_heap, max_heap)
+            else:           # 최솟값 삭제
+                if min_heap:
+                    heapq.heappop(min_heap)
+                    if not min_heap or -max_heap[0] < min_heap[0]:
+                        min_heap, max_heap = [], []
+                    print(cmd, num, "최솟값 제거 연산 후", min_heap, max_heap)
+    if not min_heap:
+        return [0, 0]
+    return [-heapq.heappop(max_heap), heapq.heappop(min_heap)]
 
 # 처음 풀이
 # Heap 안 쓰고 List로 개떡같이 짰는데 TC가 적어서인지 통과됨..
